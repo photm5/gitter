f9cf036ff84331dd3a8c22db8878d9918e8bfdb6
shak-mar <shakmar@openmailbox.org>
2014-09-15 16:07:42 +0200

    Quit correctly when an exception reaches main.py

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ import time
 import plugin_manager
 
 if __name__ == '__main__':
-    main = plugin_manager.get ( 'main' )
-    while main.active:
-        time.sleep ( 2 )
-        if main.reload_plugin:
-            plugin_manager.reload ( main.reload_plugin )
-            main.reload_plugin = None
-    plugin_manager.unload ( 'main' )
+    try:
+        main = plugin_manager.get ( 'main' )
+        while main.active:
+            time.sleep ( 2 )
+            if main.reload_plugin:
+                plugin_manager.reload ( main.reload_plugin )
+                main.reload_plugin = None
+        plugin_manager.unload ( 'main' )
+    except Exception as e:
+        print ( '===' )
+        print ( 'Exception catched in main.py:' )
+        print ( e )
+        print ( 'Unloading plugins...' )
+        print ( '===' )
+        while len ( plugin_manager.plugins ) > 0:
+            plugin_manager.unload ( list ( plugin_manager.plugins.keys () ) [ 0 ] )
+        raise e
